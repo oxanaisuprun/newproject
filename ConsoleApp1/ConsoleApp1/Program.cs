917ecb14829d0c322d5963ec1c3026ee9ff90306
oxanaisuprun <41331070+oxanaisuprun@users.noreply.github.com>
2019-11-29 10:45:13 +0200

    Update Program.cs

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace ConsoleApp1
 {
@@ -6,36 +6,35 @@ namespace ConsoleApp1
     {
         static void Main()
         {
-            String v = string.Empty;
-            double a = double.NaN;
-            double sq = double.NaN;
-            int n = 0;
+            String user_decision = string.Empty;
+            double a = 0.00;
+            double sq = 0.00;
+            Random random = new Random();
+            double random_value = random.Next(1, 5);
+            int temp_counter = 0;
             Console.WriteLine("Please select Round or Square");
             Console.WriteLine("Input r for round or s for square");
-            v = Convert.ToString(Console.ReadLine());
-            if (n < 3)
-            {
-                Console.WriteLine("Input lenght");
-                //a = Convert.ToDouble();
-                While (double.TryParse(Console.ReadLine(), out a) != true){ };
-                n++;
-            }
-            else { a = Random(0.5, 5); 
-            }
+            user_decision = Convert.ToString(Console.ReadLine());
             
-            switch (v)
+            if (temp_counter < 3 && double.TryParse(Console.ReadLine(), out _))
             {
-                case "r": sq = Math.PI * Math.Pow(a, 2);
+                Console.WriteLine("Please , input a number");
+                a = Math.Round(Convert.ToDouble(Console.ReadLine()), 2);
+                temp_counter++;
+            }
+            else { a = random_value; }
+
+
+            switch (user_decision)
+            {
+                case "r":
+                    sq = Math.Round(Math.PI * Math.Pow(a, 2), 2);
                     break;
-                case "s": sq = Math.Pow(a, 2);
+                case "s":
+                    sq = Math.Round(Math.Pow(a, 2), 2);
                     break;
             }
             Console.WriteLine("The sqare is equal to " + sq);
-        }
-
-        private static double Random(double v1, double v2)
-        {
-            throw new NotImplementedException();
         }
     }
 }
